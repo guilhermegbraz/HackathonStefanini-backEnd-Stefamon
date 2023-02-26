c8a0c8e409582df36f0b799c6819d1735ad39293
Gui <guilherme.g.braz@outlook.com>
2023-02-26 16:08:44 -0300

    Entidades Jogar

---
+++ src/main/java/com/stefanini/core/entidades/Jogador.java
@@ -0,0 +1,58 @@
+package com.stefanini.core.entidades;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+
+public class Jogador {
+
+    private Long id;
+    private String nickname;
+
+    private String senha;
+
+    private List<StefaMon> listaStefaMons;
+
+    private BigDecimal saldo;
+
+    public Jogador(String nickname, String senha, List<StefaMon> stefaMons) {
+        this.nickname = nickname;
+        this.senha = senha;
+        this.saldo = BigDecimal.valueOf(2300);
+        this.listaStefaMons = stefaMons;
+    }
+
+    public Jogador(Long id, String nickname, String senha, List<StefaMon> listaStefaMons, BigDecimal saldo) {
+        this.id = id;
+        this.nickname = nickname;
+        this.senha = senha;
+        this.saldo = saldo;
+        this.listaStefaMons = listaStefaMons;
+    }
+
+
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public List<StefaMon> getListaStefaMons() {
+        return Collections.unmodifiableList(listaStefaMons);
+    }
+
+    public void addStefamon(StefaMon stefamon) {
+        this.listaStefaMons.add(stefamon);
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
