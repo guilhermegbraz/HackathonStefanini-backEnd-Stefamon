d9eb11e75123fe449dbd8f8f483df68910e7ca75
Gui <guilherme.g.braz@outlook.com>
2023-02-26 16:44:46 -0300

    Detalhar um usuario

--- src/main/java/com/stefanini/configurations/ConfiguracaoCasosDeUso.java
+++ src/main/java/com/stefanini/configurations/ConfiguracaoCasosDeUso.java
@@ -4,6 +4,7 @@ import com.stefanini.core.casosDeUso.criarJogador.CriarJogador;
 import com.stefanini.core.casosDeUso.criarJogador.validacoes.*;
 import com.stefanini.core.casosDeUso.criptografarSenha.Criptografador;
 import com.stefanini.core.casosDeUso.criptografarSenha.CriptografarSenhaBase64;
+import com.stefanini.core.casosDeUso.detalharUmJogador.DetalharUmJogador;
 import com.stefanini.core.casosDeUso.listarJogadores.ListarJogadores;
 import com.stefanini.core.casosDeUso.parsers.JogadorToJogadorViewDto;
 import com.stefanini.core.repositorios.JogadorRepository;
@@ -39,6 +40,12 @@ public class ConfiguracaoCasosDeUso {
     @ApplicationScoped
     public ListarJogadores listarJogadores(JogadorRepository jogadorRepository) {
         return new ListarJogadores(jogadorRepository,new JogadorToJogadorViewDto());
+    }
+
+    @Produces
+    @ApplicationScoped
+    public DetalharUmJogador detalharUmJogador(JogadorRepository jogadorRepository) {
+        return new DetalharUmJogador(jogadorRepository, new JogadorToJogadorViewDto());
     }
 
 }
